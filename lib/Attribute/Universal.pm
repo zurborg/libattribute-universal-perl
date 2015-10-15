a08f7b9bf6b6a48bb5d5c62cc714e903ab469531
David Zurborg <d.zurborg@vcare24.de>
2015-10-15 08:14:33 +0200

    new function to_hash

--- lib/Attribute/Universal.pm
+++ lib/Attribute/Universal.pm
@@ -27,6 +27,92 @@ sub import {
   }
 }
 
+=func to_hash
+
+    sub ATTRIBUTE {
+        my $hash = Attribute::Universal::to_hash(@_);
+    }
+
+Its hard to remember what arguments are given to C<ATTRIBUTE()>. This helper function converts the list into a hashref, with these keywords:
+
+=over 4
+
+=item * I<package>
+
+The package the attribute was used
+
+=item * I<symbol>
+
+The GlobRef to the named symbol or the string L<LEXICAL>.
+
+=item * I<referent>
+
+The reference to the object itself (CodeRef, HashRef, ArrayRef or ScalarRef)
+
+=item * I<attribute>
+
+The name of the attribute
+
+=item * I<payload>
+
+The payload of all attributes, if used more than once. This is an ArrayRef of strings!
+
+=item * I<phase>
+
+The phase the attribute was covered. (BEGIN, CHECK, INIT, END)
+
+=item * I<file>
+
+The filename, if known
+
+=item * I<line>
+
+The linenumber, if known
+
+=back
+
+And these additional keywords:
+
+=over 4
+
+=item * I<label>
+
+The name of the symbol. Imagine you have:
+
+    sub MyFunction : Attribute;
+    our $MyScalar : Attribute;
+
+so I<label> becomes C<MyFunction> and C<MyScalar>
+
+A lexical symbol cannot have a label.
+
+=item * I<type>
+
+The reftype of the referent (CODE, HASH, ARRAY, SCALAR)
+
+=back
+
+=cut
+
+sub to_hash {
+    shift if $_[0] eq __PACKAGE__;
+    my ($package, $symbol, $referent, $attribute, $payload, $phase, $file, $line) = @_;
+    my $label = ref ($symbol) ? *{$symbol}{NAME} : undef;
+    my $type  = ref $referent;
+    return {
+      package   => $package,
+      symbol    => $symbol,
+      referent  => $referent,
+      attribute => $attribute,
+      payload   => $payload,
+      phase     => $phase,
+      file      => $file,
+      line      => $line,
+      label     => $label,
+      type      => $type,
+    };
+}
+
 1;
 
 =head1 SYNOPSIS
